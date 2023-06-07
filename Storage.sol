2002f54717f119fcfb581ac1603313e4a5607014
Vocci <vocci[Chotmail.com>
2023-06-07 11:55:57 +0200

    pragma version modified

--- Storage.sol
+++ Storage.sol
@@ -1,31 +1,30 @@
 // SPDX-License-Identifier: MIT
-    pragma solidity ^0.8.20; 
+pragma solidity ^0.8.0;
 
-    contract SimpleStorage {
+contract SimpleStorage {
+    // This gets initiallized to zero!
+    uint256 favoriteNumber;
 
-        // This gets initiallized to zero!
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    struct People {
         uint256 favoriteNumber;
-
-        mapping(string => uint256) public nameToFavoriteNumber;
-
-        struct People {
-            uint256 favoriteNumber;
-            string name;
-        }
-
-        People[] public people;
-
-        function store(uint256 _favoriteNumber) public {
-            favoriteNumber = _favoriteNumber;
-        }
-
-        function retrieve() public view returns (uint256) {
-            return favoriteNumber;
-        }
-
-        // calldata, memory, storage to specify only for strings(string is an array of bytes), arrays, struct or mappings
-        function addPerson(string memory _name, uint256 _favoriteNumber) public {
-            people.push(People(_favoriteNumber, _name));
-            nameToFavoriteNumber[_name] = _favoriteNumber;
-        }
+        string name;
     }
+
+    People[] public people;
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return favoriteNumber;
+    }
+
+    // calldata, memory, storage to specify only for strings(string is an array of bytes), arrays, struct or mappings
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+}
